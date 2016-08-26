a94dd04719304704d19cd08e6af5422bfe7bab3c
Gerard Casas <gerard.casas.saez@est.fib.upc.edu>
2016-08-26 02:46:10 +0200

    solve leaves disappearing before than expected

--- src/scripts/leaves.js
+++ src/scripts/leaves.js
@@ -83,7 +83,7 @@ function update()
         
         //Sending flakes back from the top when it exits
         //Lets make it a bit more organic and let flakes enter from the left and right also.
-        if(p.x > W+maxSize*2 || p.x < -5 || p.y > H-maxSize*2)
+        if(p.x > W+maxSize*2 || p.x < -maxSize*2 || p.y > H)
         {
             if(i%3 > 0) //66.67% of the flakes
             {
