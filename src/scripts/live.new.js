eb41e40e8453009f7b6fb4a0385c8be8f03d8ffb
Marcelino Coll Rovira <coll.marce@gmail.com>
2016-10-02 18:40:58 +0200

    Added live

--- src/scripts/live.new.js
+++ src/scripts/live.new.js
@@ -1,4 +1,6 @@
 var leaves;
+var testing = true;
+var first = true;
 
 var app = new Vue({
   el: '#app',
@@ -9,7 +11,8 @@ var app = new Vue({
     eventsNotify: true,
     bieneNotify: false,
     timeline: null,
-    animation: true
+    animation: true,
+    dateFormat: "%H:%M:%S"
   },
   methods: {
     options: function() {
@@ -18,13 +21,35 @@ var app = new Vue({
     updateTimetable: function () {
     this.$http.get('/assets/data/events.json')
       .then(function(response) {
-        if(response.body.events != this.$get('timeline')) {
-          this.$set('timeline', response.body.events);
+        timeline = this.$get('timeline')
+        if(response.body.events != timeline) {
+          events = response.body.events;
+
+          if(testing && first) {
+            first = false;
+            for(var i = 0; i < events.length; i++) {
+              events[i].begin = new Date(Date.now() + i*30000);
+              events[i].end = new Date(Date.now() + i*60000);
+            }
+          } else if(!testing) {
+            for(var i = 0; i < events.length; i++) {
+              events[i].begin = new Date(events[i].begin);
+              events[i].end = new Date(events[i].end);
+            }
+          } else {
+            for(var i = 0; i < events.length; i++) {
+              events[i].begin = timeline[i].begin;
+              events[i].end = timeline[i].end;
+            }
+          }
+
+          this.$set('timeline', events);
         }
       }, function(response) {
         console.log("Sth wrong");
       });
-    }, 
+    },
+    // Toggles animation between stopped and started state
     toggleAnimation: function (data) {
       console.log("toggle");
       if(data == false) {
@@ -33,9 +58,22 @@ var app = new Vue({
         leaves.restart();
       }
     },
+    // Starts background animation
     startAnimation: function () {
       leaves = new Leaves(true);
       leaves.start();
+    },
+    // Returns false if not in range and percentage of completion if true
+    whereAreWe: function (start, end) {
+      d = new Date(Date.now());
+      
+      if(start <= d && d <= end) { // Inside the range
+        return (d - start)/(end - start);
+      } else if(d < start) {
+        return -2; // Before the range
+      } else if(d > end) {
+        return -1; // After the range
+      }
     }
   },
   watch: {
@@ -43,6 +81,24 @@ var app = new Vue({
       this.toggleAnimation(data);
     }
   },
+  computed: {
+    completedEvents: function() {
+      return this.timeline.filter(function(data) {
+        return app.whereAreWe(data.begin, data.end) === -1;
+      }).slice(-5);
+    },
+    currentEvents: function() {
+      return this.timeline.filter(function(data) {
+        percentage = app.whereAreWe(data.begin, data.end);
+        return percentage > 0;
+      });
+    },
+    futureEvents: function() {
+      return this.timeline.filter(function(data) {
+        return app.whereAreWe(data.begin, data.end) === -2;
+      });
+    }
+  }
 });
 
 app.updateTimetable();
@@ -50,4 +106,4 @@ app.startAnimation();
 
 window.setInterval(function(){
   app.updateTimetable();
-}, 15000);
+}, 5000);
