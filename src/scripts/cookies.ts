a94da4493e48c8a068f0a90b6408591f7d8e9f3d
Maurici <hello@mauriciabad.com>
2019-06-15 13:36:22 +0200

    Cookies changed button OK for x

--- src/scripts/cookies.ts
+++ src/scripts/cookies.ts
@@ -1,10 +1,12 @@
 // const cookieText = 'This website uses cookies of its own and of third parties to improve your browsing experience and show you personalized content based on your interests. If you continue browsing, we consider that you accept its use. You can obtain more information in our <a class="cookie-notice__link" href="/privacy_and_cookies" target="_blank">Privacy and Cookies Policy</a>'
-const cookieText = 'This website uses cookies on your device to analyze site usage. <a class="cookie-notice__link" href="/privacy_and_cookies" target="_blank">Privacy and Cookies Policy</a>'
+// const cookieText = 'This website uses cookies to ensure you get the best experience <a class="cookie-notice__link" href="/privacy_and_cookies" target="_blank">More info</a>'
+// const cookieText = 'This website uses cookies to analyze site usage. <a class="cookie-notice__link" href="/privacy_and_cookies" target="_blank">Privacy and Cookies Policy</a>'
+const cookieText = 'We uses cookies to analyze site usage. <a class="cookie-notice__link" href="/privacy_and_cookies" target="_blank">More info</a>'
 const cookieNotice = document.createElement('div')
 cookieNotice.innerHTML = `<p class="cookies-notice__text">${cookieText}</p><button class="cookies-notice__button" onclick="acceptCookies()">OK</button>`
 cookieNotice.classList.add('cookies-notice')
 
-if (window.localStorage.getItem('cookiesAccepted') !== 'true') {
+if (window.localStorage.getItem('cookies') !== '1') {
   if (document.readyState !== 'loading') {    
     document.body.appendChild(cookieNotice)
   }else{
@@ -15,6 +17,6 @@ if (window.localStorage.getItem('cookiesAccepted') !== 'true') {
 }
 
 function acceptCookies () {
-  window.localStorage.setItem('cookiesAccepted', 'true')
+  window.localStorage.setItem('cookies', '1')
   cookieNotice.classList.add('cookies-notice--hidden')
 }
