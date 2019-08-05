8de9a92a5762152dbd04b38a5372260e3131e0a9
Maurici Abad <hello@mauriciabad.com>
2019-08-05 14:46:42 +0200

    Autofix js files

--- src/scripts/config.live.js
+++ src/scripts/config.live.js
@@ -1,26 +1,26 @@
 var CONST = {
-  'FAKE_DATE': new Date('2018-10-19 21:15'),
-  'FADE_CLASS': 'faded',
-  'HIDE_CLASS': 'hidden',
-  'VEIL_CLASS': 'veil',
-  'VEILED_CLASS': 'veiled',
-  'SELECTED_CLASS': 'selected',
-  'ACTIVE_CLASS': 'active',
-  'HAPPENING_CLASS': 'happening',
-  'HAPPENED_CLASS': 'happened',
-  // Time in seconds for every step
-  'SCHEDULE_STEP': 3600,
-  // ms of fade animation (should match params.less)
-  'FADE_TIME': 300,
-  // ms of aside opening animation (should match params.less)
-  'ASIDE_OPEN_TIME': 300,
-  // ms to timeout an async req
-  'REQ_TIMEOUT': 2000,
-  'SCHEDULE_REFRESH_INTERVAL': 60000,
-  'DEFAULT_VIEW': 'live',
-  'HACKATHON_DURATION': 36 * 60 * 60,
-  'DEFAULT_NOTIFICATION_TITLE': 'HackUPC Fall 2017',
-  'DEFAULT_NOTIFICATION_ICON': 'favicon.ico',
-  'NOTIFICATION_TIMEOUT': 7000,
-  'EVENT_NOTIF_OFFSET': 5 * 60
+	'FAKE_DATE': new Date('2018-10-19 21:15'),
+	'FADE_CLASS': 'faded',
+	'HIDE_CLASS': 'hidden',
+	'VEIL_CLASS': 'veil',
+	'VEILED_CLASS': 'veiled',
+	'SELECTED_CLASS': 'selected',
+	'ACTIVE_CLASS': 'active',
+	'HAPPENING_CLASS': 'happening',
+	'HAPPENED_CLASS': 'happened',
+	// Time in seconds for every step
+	'SCHEDULE_STEP': 3600,
+	// ms of fade animation (should match params.less)
+	'FADE_TIME': 300,
+	// ms of aside opening animation (should match params.less)
+	'ASIDE_OPEN_TIME': 300,
+	// ms to timeout an async req
+	'REQ_TIMEOUT': 2000,
+	'SCHEDULE_REFRESH_INTERVAL': 60000,
+	'DEFAULT_VIEW': 'live',
+	'HACKATHON_DURATION': 36 * 60 * 60,
+	'DEFAULT_NOTIFICATION_TITLE': 'HackUPC Fall 2017',
+	'DEFAULT_NOTIFICATION_ICON': 'favicon.ico',
+	'NOTIFICATION_TIMEOUT': 7000,
+	'EVENT_NOTIF_OFFSET': 5 * 60
 }
--- src/scripts/util.live.js
+++ src/scripts/util.live.js
@@ -1,167 +1,167 @@
 var Util = (function (CONST) {
-  'use strict'
-  if (!CONST) {
-    console.error('CONST module not found at this point')
-    return null
-  }
+	'use strict'
+	if (!CONST) {
+		console.error('CONST module not found at this point')
+		return null
+	}
 
-  var obj = {}
-  /*
+	var obj = {}
+	/*
   * Loads a file asynchronously
   * if succesfull calls cb(response),
   * else calls cberr(statusText)
   */
-  obj.loadFile = function (sURL, cb, cberr) {
-    var oReq = new window.XMLHttpRequest()
-    oReq.onload = function () {
-      if (this.readyState === 4) {
-        if (this.status === 200) {
-          if (typeof cb === 'function') { cb(this.responseText) }
-        } else {
-          if (typeof cberr === 'function') { cberr(this.statusText) }
-        }
-      }
-    }
-    oReq.onerror = function () {
-      if (typeof cberr === 'function') { cberr(this.statusText) }
-    }
-    oReq.open('GET', sURL, true)
-    oReq.timeout = CONST.REQ_TIMEOUT
-    oReq.send(null)
-  }
+	obj.loadFile = function (sURL, cb, cberr) {
+		var oReq = new window.XMLHttpRequest()
+		oReq.onload = function () {
+			if (this.readyState === 4) {
+				if (this.status === 200) {
+					if (typeof cb === 'function') { cb(this.responseText) }
+				} else {
+					if (typeof cberr === 'function') { cberr(this.statusText) }
+				}
+			}
+		}
+		oReq.onerror = function () {
+			if (typeof cberr === 'function') { cberr(this.statusText) }
+		}
+		oReq.open('GET', sURL, true)
+		oReq.timeout = CONST.REQ_TIMEOUT
+		oReq.send(null)
+	}
 
-  /*
+	/*
   * Faded elements are rendered but not visible (opacity: 0)
   */
-  obj.fadeOut = function (element, cb) {
-    element.classList.add(CONST.FADE_CLASS)
-    setTimeout(function () {
-      if (typeof cb === 'function') { cb() }
-    }, CONST.FADE_TIME)
-  }
+	obj.fadeOut = function (element, cb) {
+		element.classList.add(CONST.FADE_CLASS)
+		setTimeout(function () {
+			if (typeof cb === 'function') { cb() }
+		}, CONST.FADE_TIME)
+	}
 
-  obj.fadeIn = function (element, cb) {
-    element.classList.remove(CONST.FADE_CLASS)
-    setTimeout(function () {
-      if (typeof cb === 'function') { cb() }
-    }, CONST.FADE_TIME)
-  }
+	obj.fadeIn = function (element, cb) {
+		element.classList.remove(CONST.FADE_CLASS)
+		setTimeout(function () {
+			if (typeof cb === 'function') { cb() }
+		}, CONST.FADE_TIME)
+	}
 
-  /*
+	/*
   * Hidden elements are not rendered (display: none)
   */
-  obj.hide = function (element) {
-    element.classList.add(CONST.HIDE_CLASS)
-  }
+	obj.hide = function (element) {
+		element.classList.add(CONST.HIDE_CLASS)
+	}
 
-  obj.show = function (element) {
-    element.classList.remove(CONST.HIDE_CLASS)
-  }
+	obj.show = function (element) {
+		element.classList.remove(CONST.HIDE_CLASS)
+	}
 
-  /*
+	/*
   * Veiled objects are covered with a semi-transparent
   * color
   */
-  obj.veil = function (element, cb) {
-    element.classList.add(CONST.VEIL_CLASS)
+	obj.veil = function (element, cb) {
+		element.classList.add(CONST.VEIL_CLASS)
 
-    // Force dom repaint
-    setTimeout(function () {
-      element.classList.add(CONST.VEILED_CLASS)
-    }, 1)
-    setTimeout(function () {
-      if (typeof cb === 'function') { cb() }
-    }, CONST.FADE_TIME)
-  }
-  obj.unveil = function (element, cb) {
-    element.classList.remove(CONST.VEILED_CLASS)
-    setTimeout(function () {
-      element.classList.remove(CONST.VEIL_CLASS)
-      if (typeof cb === 'function') { cb() }
-    }, CONST.FADE_TIME)
-  }
+		// Force dom repaint
+		setTimeout(function () {
+			element.classList.add(CONST.VEILED_CLASS)
+		}, 1)
+		setTimeout(function () {
+			if (typeof cb === 'function') { cb() }
+		}, CONST.FADE_TIME)
+	}
+	obj.unveil = function (element, cb) {
+		element.classList.remove(CONST.VEILED_CLASS)
+		setTimeout(function () {
+			element.classList.remove(CONST.VEIL_CLASS)
+			if (typeof cb === 'function') { cb() }
+		}, CONST.FADE_TIME)
+	}
 
-  /*
+	/*
   * Sometimes we want to prevent scroll
   * in an element
   */
 
-  obj.blockScroll = function (element) {
-    element.style.overflow = 'hidden'
-  }
+	obj.blockScroll = function (element) {
+		element.style.overflow = 'hidden'
+	}
 
-  obj.releaseScroll = function (element) {
-    element.style.overflow = 'auto'
-  }
+	obj.releaseScroll = function (element) {
+		element.style.overflow = 'auto'
+	}
 
-  /*
+	/*
   * Adds an optional 0 padding upfront
   * Useful for displaying hours
   */
-  obj.pad = function (number) {
-    return ('0' + number).slice(-2)
-  }
+	obj.pad = function (number) {
+		return ('0' + number).slice(-2)
+	}
 
-  /*
+	/*
   * HH:MM format hour to seconds (delta)
   */
-  obj.hourStringToSeconds = function (hour) {
-    var hp = hour.split(':')
-    return hp[0] * 60 * 60 + hp[1] * 60
-  }
+	obj.hourStringToSeconds = function (hour) {
+		var hp = hour.split(':')
+		return hp[0] * 60 * 60 + hp[1] * 60
+	}
 
-  /*
+	/*
   * Seconds passed between epoch and 'date'
   */
-  obj.dateStringToSeconds = function (d) {
-    var dateFormat = /^([0-3]?\d)\W([0-1]?\d)\W(\d{4})(\W([0-2]?\d)\W([0-5]?\d))?$/
-    var result = d.match(dateFormat)
-    return Date.UTC(result[3], result[2] - 1, result[1],
-      result[6] || 0, result[5] || 0) / 1000
-  }
+	obj.dateStringToSeconds = function (d) {
+		var dateFormat = /^([0-3]?\d)\W([0-1]?\d)\W(\d{4})(\W([0-2]?\d)\W([0-5]?\d))?$/
+		var result = d.match(dateFormat)
+		return Date.UTC(result[3], result[2] - 1, result[1],
+			result[6] || 0, result[5] || 0) / 1000
+	}
 
-  var realStartDate = new Date()
+	var realStartDate = new Date()
 
-  obj.getNowSeconds = function () {
-    if (CONST.FAKE_DATE) { return (CONST.FAKE_DATE.getTime() + Date.now() - realStartDate.getTime()) / 1000 } else { return Date.now() / 1000 }
-  }
+	obj.getNowSeconds = function () {
+		if (CONST.FAKE_DATE) { return (CONST.FAKE_DATE.getTime() + Date.now() - realStartDate.getTime()) / 1000 } else { return Date.now() / 1000 }
+	}
 
-  obj.getNowDate = function () {
-    return new Date(obj.getNowSeconds() * 1000)
-  }
+	obj.getNowDate = function () {
+		return new Date(obj.getNowSeconds() * 1000)
+	}
 
-  obj.getHumanTime = function (s) {
-    return {
-      seconds: parseInt(s % 60),
-      minutes: parseInt(s / 60 % 60),
-      hours: parseInt(s / (60 * 60))
-    }
-  }
+	obj.getHumanTime = function (s) {
+		return {
+			seconds: parseInt(s % 60),
+			minutes: parseInt(s / 60 % 60),
+			hours: parseInt(s / (60 * 60))
+		}
+	}
 
-  /*
+	/*
   * Given a template id, replaces '{key}' with 'data.key'
   * and returns a clone
   */
-  obj.inflateWith = function (templateId, data) {
-    var element = document.getElementById(templateId)
-    var clone = element.cloneNode(true)
-    clone.innerHTML = clone.innerHTML.replace(/\{(.+?)\}/g, function (full, label) {
-      return data[label] == null ? '' : data[label]
-    })
+	obj.inflateWith = function (templateId, data) {
+		var element = document.getElementById(templateId)
+		var clone = element.cloneNode(true)
+		clone.innerHTML = clone.innerHTML.replace(/\{(.+?)\}/g, function (full, label) {
+			return data[label] == null ? '' : data[label]
+		})
 
-    return document.importNode(clone.content, true)
-  }
+		return document.importNode(clone.content, true)
+	}
 
-  obj.storageGet = function (key) {
-    var storage = JSON.parse(window.localStorage['appData'] || '{}')
-    return storage[key] ? storage[key] : null
-  }
+	obj.storageGet = function (key) {
+		var storage = JSON.parse(window.localStorage['appData'] || '{}')
+		return storage[key] ? storage[key] : null
+	}
 
-  obj.storagePut = function (key, value) {
-    var storage = JSON.parse(window.localStorage['appData'] || '{}')
-    storage[key] = value
-    window.localStorage['appData'] = JSON.stringify(storage)
-  }
+	obj.storagePut = function (key, value) {
+		var storage = JSON.parse(window.localStorage['appData'] || '{}')
+		storage[key] = value
+		window.localStorage['appData'] = JSON.stringify(storage)
+	}
 
-  return obj
+	return obj
 })(CONST)
