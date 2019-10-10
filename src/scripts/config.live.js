9c723df1e03c13f5480763748fd56b949003ef28
Maurici <hello@mauriciabad.com>
2019-10-10 15:44:33 +0200

    Hide fakedate

--- src/scripts/config.live.js
+++ src/scripts/config.live.js
@@ -1,6 +1,6 @@
 // eslint-disable-next-line no-unused-vars
 var CONST = {
-	'FAKE_DATE': new Date('2019-10-11 15:59:55'),
+	// 'FAKE_DATE': new Date('2019-10-11 15:59:55'),
 	'FADE_CLASS': 'faded',
 	'HIDE_CLASS': 'hidden',
 	'VEIL_CLASS': 'veil',
