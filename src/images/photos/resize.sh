4e1f807d2cf8e7e5fb4e0516efe961a9ce65b27c
Maurici <hello@mauriciabad.com>
2020-02-13 00:14:11 +0100

    Optimize photos fix

--- src/images/photos/resize.sh
+++ src/images/photos/resize.sh
@@ -4,8 +4,8 @@ for file in *.jpg; do
     for size in 25 50 100 200 400 600 800; do
         convert "$file" -resize $size multisize/"${file%.*}"-"$size"px.jpg
         echo "Resized multisize/"${file%.*}"-"$size"px.jpg"
+        jpegoptim "multisize/"${file%.*}"-"$size"px.jpg"
+        echo "Optimized multisize/"${file%.*}"-"$size"px.jpg"
     done
 done
-echo "Optimazing images..."
-jpegoptim multisize/*.jpeg
 echo "Done"
