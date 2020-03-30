901837fc6f695d62803ee57f7d7091315acafb26
Maurici <hello@mauriciabad.com>
2020-03-31 00:22:42 +0200

    Add Lax.js

--- src/index.js
+++ src/index.js
@@ -4,4 +4,15 @@
 // import './styles/style.sass';
 import 'normalize.css';
 import './styles/style.scss';
+import lax from 'lax.js'
 
+window.onload = function() {
+	lax.setup() // init
+
+	const updateLax = () => {
+		lax.update(window.scrollY)
+		window.requestAnimationFrame(updateLax)
+	}
+
+	window.requestAnimationFrame(updateLax)
+}
