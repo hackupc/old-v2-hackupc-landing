08dedaf907b4562aeb7dd867fc533095d7e1f948
mauriciabad <hello@mauriciabad.com>
2020-04-09 01:52:06 +0200

    Update npm packages

--- config/webpack.dev.js
+++ config/webpack.dev.js
@@ -1,7 +1,6 @@
 const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 const { CleanWebpackPlugin } = require("clean-webpack-plugin");
-const RealFaviconPlugin = require('real-favicon-webpack-plugin');
 
 module.exports = {
   entry: {
