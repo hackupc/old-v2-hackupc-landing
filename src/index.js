c47e3d719fafee1b0115d17ee0813f92ce295c38
mauriciabad <hello@mauriciabad.com>
2020-05-06 00:10:50 +0200

    Half "During the event done"

--- src/index.js
+++ src/index.js
@@ -12,7 +12,7 @@ import webfont from 'webfontloader';
  
 webfont.load({
 	google: {
-		families: ['Montserrat:400,400i,600,700,800&display=swap']
+		families: ['Montserrat:400,400i,600,700,800,800i&display=swap']
 	}
 });
 	
