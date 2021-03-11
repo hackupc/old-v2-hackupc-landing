19aa6030ac7b6c323731701b27420e605f161f89
Maurici Abad <maurici.abad@glovoapp.com>
2021-03-11 22:54:59 +0100

    New hero WIP

--- src/index.ts
+++ src/index.ts
@@ -6,4 +6,5 @@ import('focus-visible')
 import './scripts/fonts.ts'
 import './scripts/analytics.ts'
 import './scripts/cookies.ts'
+import './scripts/hero.ts'
 import './scripts/faq.ts'
---
+++ src/scripts/hero.ts
@@ -0,0 +1,21 @@
+const circleElem: HTMLElement | null = document.querySelector('#circle')
+const heroBgElem: HTMLElement | null = document.querySelector('.hero-bg')
+
+if (circleElem && heroBgElem) {
+  const circleRadius = {
+    x: circleElem.offsetWidth / 2,
+    y: circleElem.offsetHeight / 2,
+  }
+
+  heroBgElem.addEventListener('mousemove', function (event) {
+    const heroBgRect = heroBgElem.getBoundingClientRect()
+    circleElem.style.display = ''
+    circleElem.style.transform = `translate(${Math.floor(
+      event.clientX - heroBgRect.left - circleRadius.x
+    )}px, ${Math.floor(event.clientY - heroBgRect.top - circleRadius.y)}px)`
+  })
+
+  heroBgElem.addEventListener('mouseout', function () {
+    circleElem.style.display = 'none'
+  })
+}
