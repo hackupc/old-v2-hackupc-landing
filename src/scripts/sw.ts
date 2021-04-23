712ac44da214deffaee3cbe973a32f034ae3ce4b
Maurici Abad <maurici.abad@glovoapp.com>
2021-04-23 18:38:43 +0200

    fix

--- src/scripts/sw.ts
+++ src/scripts/sw.ts
@@ -1,8 +1,6 @@
 if ('serviceWorker' in navigator) {
   window.addEventListener('load', async () => {
-    await navigator.serviceWorker.register(
-      '/service-worker.js'
-    )
+    await navigator.serviceWorker.register('/service-worker.js')
   })
 }
 
