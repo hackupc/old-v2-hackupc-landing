c821b59544f7dadd8d3d6d2c250efc0b9e32b6b8
Maurici Abad <hello@mauriciabad.com>
2021-12-16 22:28:59 +0100

    skip waiting service worker

--- src/scripts/sw.ts
+++ src/scripts/sw.ts
@@ -1,8 +1,14 @@
+/*
 if ('serviceWorker' in navigator) {
   window.addEventListener('load', async () => {
     await navigator.serviceWorker.register('/service-worker.js')
   })
 }
+*/
+
+self.addEventListener('install', function () {
+  self.skipWaiting()
+})
 
 window.addEventListener('beforeinstallprompt', (event) => {
   event.preventDefault()
