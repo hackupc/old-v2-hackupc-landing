e3ab8ed2de254602165bdcac94a74af0b59c6a2e
Maurici Abad Gutierrez <hello@mauriciabad.com>
2021-12-17 01:55:48 +0100

    Fix sw

--- src/scripts/sw.ts
+++ src/scripts/sw.ts
@@ -1,12 +1,10 @@
+/*
 if ('serviceWorker' in navigator) {
   window.addEventListener('load', async () => {
     await navigator.serviceWorker.register('/service-worker.js')
   })
 }
-
-self.addEventListener('install', function () {
-  self.skipWaiting()
-})
+*/
 
 window.addEventListener('beforeinstallprompt', (event) => {
   event.preventDefault()
